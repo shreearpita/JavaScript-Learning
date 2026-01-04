5c9138e4483c62897e0c90adc09405d29539cd94
Arpita Shree <shreearpita24@gmail.com>
2026-01-04 10:22:21 +0000

    variables done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,12 @@
 const accountId = 112233 //This value cannot be changed since it is a const
 
-let accountEmail = "abc@gmail.com"
+let accountEmail = "abc@gmail.com" //generally used because of scopes of variable
 
-var accountPassword = "123456"
+var accountPassword = "123456" //Prefer not to use var because of issue in block scope anf functional scope
 
-accountCity = "Jaipur"
+let accountState; // undefined value
+
+accountCity = "Jaipur" //undeclared variable : not declared using keywords like const, let, var
 
 //accountId = 35 -> Not allowed, since it is a const.
 
