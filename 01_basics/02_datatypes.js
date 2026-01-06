920be072e62c8dbe7298d504b2a25415cf504d47
Arpita Shree <shreearpita24@gmail.com>
2026-01-06 02:06:11 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,4 +2,23 @@
 
 //alert(3+3) //used in inspect console not in nodejs
 
+let name = "Gojo Satoru" //string
+let age = 28 //numeric
+let isLoggedIn = false //Boolean. Used in Login
+let state = null
 
+// Types of Datatypes
+
+/* Primitive - Datatypes
+   1. Number
+   2. bigint : trading,stockmarket,reddit etc.
+   3. string : ""
+   4. boolean : true/false
+   5. null : standalone value
+   6. undefined : value is not defined for the variable.
+   7. symbol : mostly used with react to find uniqueness.
+   
+   Object */
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
