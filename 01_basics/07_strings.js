f0e8162426c47bb6dc90775a59e7ab42c8589148
Arpita Shree <shreearpita24@gmail.com>
2026-01-07 05:17:03 +0000

    strings

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,10 @@
+const name = "Tanjiro"
+const demonCount = 25
+
+//console.log(name + demonCount + "Value"); //outdated syntax
+
+console.log(`Hello my name is ${name} and I've killed ${demonCount} demons till now.`); //String interpolation : creates placeholder to inject the variables directly. This helps apply method to the strings directly.
+
+//Declaration of string
+
+const animeName = new String('Demon Slayer') //using objects
