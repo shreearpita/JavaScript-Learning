6cafcd2c78cd5ec6273364421c53bfb6ee952fcd
Arpita Shree <shreearpita24@gmail.com>
2026-01-08 01:53:51 +0000

    strings

--- 01_basics/07_strings.js
+++ 01_basics/07_strings.js
@@ -7,4 +7,45 @@ console.log(`Hello my name is ${name} and I've killed ${demonCount} demons till 
 
 //Declaration of string
 
-const animeName = new String('Demon Slayer') //using objects
+const animeName = new String('Demon-Slayer') //using objects
+
+//Accessing key-value pair
+console.log(animeName[0]);
+
+//Accessing the prototype
+console.log(animeName.__proto__); //output : {} => empty object
+
+console.log(animeName.length);
+console.log(animeName.toLocaleUpperCase()); // Doesn't make changes to the original value but in the copy {stack-heap concept}
+console.log(animeName.charAt(2)); // which character is at which position
+console.log(animeName.indexOf('l'));
+
+//Slicing
+const newString = animeName.substring(0, 4)
+console.log(newString);
+
+const anotherString = animeName.slice(-10, 4)
+console.log(anotherString);
+
+//Trim n Replace
+
+const newStringOne = "   zenitsu   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://jjk.com/gojo%20satoru" // url coding : " " => %20
+
+console.log(url.replace('%20','-'));
+
+console.log
+(url.includes('jujutsu'));
+
+//Split
+console.log(animeName.split('-'));
+
+
+
+
+
+
+
