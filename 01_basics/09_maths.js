764ef15a70d2912ba072f54b7e2480d38551c823
Arpita Shree <shreearpita24@gmail.com>
2026-01-09 02:15:12 +0000

    maths

---
+++ 01_basics/09_maths.js
@@ -0,0 +1,7 @@
+console.log(Math);
+console.log(Math.abs(-4)); //only on negative values
+console.log(Math.round(4.89));
+console.log(Math.ceil(4.2));//top value
+console.log(Math.floor(4.2));// lower value
+console.log(Math.min(4, 6, 8, 2));
+console.log(Math.max(4, 6, 8, 2));
