87cd50863f3b431f00ccf8a1eafd038852e9b753
Arpita Shree <shreearpita24@gmail.com>
2026-01-11 12:34:17 +0000

    methods

--- 02_arrays-objects/01_arrays.js
+++ 02_arrays-objects/01_arrays.js
@@ -1,10 +1,44 @@
-// Array
+// Arrays
 
 const myArr = [0,1,2,3,4,5]
 const myDemon = ["Muzan", "Akaza", "Rui","Nezuko"]
 
 const myArr2 = new Array(1,2,3,4)
 
-console.log(myArr[1]);
+//console.log(myArr[1]);
 
 // Array Methods
+myArr.push(6) // appends into the array
+myArr.pop() //removes last value from the array
+
+myArr.unshift(9) // the value is inserted at 0 index
+myArr.shift() // removes the 0th index value
+
+// Questionaire results
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3) 
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ", myArr);
+
+console.log(myn2);
+
+
+
+
+
+
