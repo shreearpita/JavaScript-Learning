eb50c759372768f9a2ef94fa0d5849c4e9281cf7
Arpita Shree <shreearpita24@gmail.com>
2026-01-12 04:55:14 +0000

    objects

---
+++ 02_arrays-objects/03_objects.js
@@ -0,0 +1,20 @@
+//Object Constructor
+//Object.create // singleton
+
+// Object Literals
+
+const JsUser = {
+    name: "Geto Suguru",
+    age: 27,
+    location: "Shibuya",
+    email: "getosuguru@jjk.com",
+    isLoggedIn: false ,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+
+
+
+
