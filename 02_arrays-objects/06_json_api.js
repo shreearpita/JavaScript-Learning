295b55d5048fb0bee03a279055842171f873abdf
Arpita Shree <shreearpita24@gmail.com>
2026-01-13 14:21:01 +0000

    json api

---
+++ 02_arrays-objects/06_json_api.js
@@ -0,0 +1,21 @@
+// JSON : keys-values are string
+
+//object format API
+
+// {
+//     name: "Nanami Kento",
+//     technique: "1:7 Ratio",
+//     grade: "Grade-1"
+// }
+
+// array format API
+
+[
+    {},
+    {},
+    {}
+]
+
+//{"results":[{"gender":"female","name":{"title":"Madame","first":"Jutta","last":"David"},"location":{"street":{"number":5381,"name":"Rue de L'Abbé-Soulange-Bodin"},"city":"Les Enfers","state":"Fribourg","country":"Switzerland","postcode":9559,"coordinates":{"latitude":"-60.5705","longitude":"74.7442"},"timezone":{"offset":"+9:00","description":"Tokyo, Seoul, Osaka, Sapporo, Yakutsk"}},"email":"jutta.david@example.com","login":{"uuid":"736e8974-ceac-4c3f-8394-da9ffdf40a2b","username":"crazyswan688","password":"maxi","salt":"3OWVm0sm","md5":"b103d07b593a799ee177ddcfa7271764","sha1":"d95e5e0ef5848613cf7d6d3c99871e2ed8d22c89","sha256":"628d4c8777ed8dbbd1e6104104fe92d592f8e52ac54de6e2aa11e42f5434984c"},"dob":{"date":"1979-06-21T12:43:11.509Z","age":46},"registered":{"date":"2022-03-03T03:25:46.782Z","age":3},"phone":"076 061 72 68","cell":"076 373 04 42","id":{"name":"AVS","value":"756.1461.5926.78"},"picture":{"large":"https://randomuser.me/api/portraits/women/93.jpg","medium":"https://randomuser.me/api/portraits/med/women/93.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/93.jpg"},"nat":"CH"}],"info":{"seed":"62baaf6273c5d95e","results":1,"page":1,"version":"1.4"}}
+
+// Reference : randomuser me api, JSON Formattor (tree format)
