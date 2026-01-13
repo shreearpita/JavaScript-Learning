7a7faa53d32aca9fa5c95a55a516b9e47a1ccf15
Arpita Shree <shreearpita24@gmail.com>
2026-01-13 02:44:15 +0000

    objects

--- 02_arrays-objects/03_objects.js
+++ 02_arrays-objects/03_objects.js
@@ -3,8 +3,11 @@
 
 // Object Literals
 
+const mySym = Symbol("key1")
+
 const JsUser = {
     name: "Geto Suguru",
+    [mySym]: "myKey1", // type is not symbol, it's string.
     age: 27,
     location: "Shibuya",
     email: "getosuguru@jjk.com",
@@ -12,8 +15,25 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "getogojo@jjk.com"
+//Object.freeze(JsUser) //changes can't be made.
+JsUser.email = "getosugu@jjk.com"
+//console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+    
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`); // String Interpolation
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
+
 
 
 
