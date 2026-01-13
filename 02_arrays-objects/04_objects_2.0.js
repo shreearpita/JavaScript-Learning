ea5b54edbf0133bcb0ef068b7aa1d4e4d02586c6
Arpita Shree <shreearpita24@gmail.com>
2026-01-13 13:46:07 +0000

    objects_2.0

--- 02_arrays-objects/04_objects_2.0.js
+++ 02_arrays-objects/04_objects_2.0.js
@@ -24,3 +24,48 @@ console.log(regularUser.fullname.userfullname.firstname);
 
 // Combining Objects
 
+const obj1 = {1: "a",2: "b"}
+const obj2 = {3: "a",4: "b"}
+const obj4 = {5: "a",6: "b"}
+
+//const obj3 = {obj1, obj2}
+//const obj3 = Object.assign({}, obj1, obj2, obj4)// {} : target 
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3); //object under object
+
+
+//Values from Database
+
+const users = [
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+]
+
+users[1].email
+
+console.log(animeLovers);
+
+console.log(Object.keys(animeLovers)); // all keys in arrays
+console.log(Object.values(animeLovers));
+console.log(Object.entries(animeLovers)); // every key-value pair in array
+
+//Avoid crashing if value is not present
+
+console.log(animeLovers.hasOwnProperty('isLogged'));
+
+
