62b6ad18430a36a10dcfc6cf4fe2a0b4f756d47b
Arpita Shree <shreearpita24@gmail.com>
2026-01-16 10:43:56 +0000

    industry login function

--- 03_functions/01_func.js
+++ 03_functions/01_func.js
@@ -10,16 +10,40 @@ function sayMyName(){
 sayMyName() //Function execution
 //sayMyName // Function reference : no error in runtime
 
-function addition(num1, num2){ 
+// function addition(num1, num2){ 
 
-    console.log(num1 + num2);
+//     console.log(num1 + num2);
+    
+// }
+//addition() // NaN, because argument was not passed.
+//addition(4,"8") // 48
+
+// const result = addition (5,3)
+// console.log("Result: ", result); // undefined
+
+function addition(num1, num2){ 
+     
+    // let result = num1 + num2;
+    // return result // end statement 
+    return num1 + num2
     
 }
-addition() // NaN, because argument was not passed.
-addition(4,"8") // 48
 
-const result = addition (5,3)
-console.log("Result: ", result); // undefined
+const result = addition(5,3)
 
+console.log("Result : ", result);
 
+// Printing User Message
 
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+//console.log(loginUserMessage("Gojo Saturo"))
+console.log(loginUserMessage("Nanami Kento"))
+
+// if(username === undefined) : if(!username)
