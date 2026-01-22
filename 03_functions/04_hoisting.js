7edf23f0c03e0207bd374c7ba5e06aa2bc6b8560
Arpita Shree <shreearpita24@gmail.com>
2026-01-22 02:00:51 +0000

    nested scope

---
+++ 03_functions/04_hoisting.js
@@ -0,0 +1,12 @@
+// Closure, DOM : Document Object Model
+
+// Nested Scope
+
+function one(){
+    const username = "Nobara Kugisaki"
+
+    function two(){
+        const grade = "Three"
+    }
+}
+
