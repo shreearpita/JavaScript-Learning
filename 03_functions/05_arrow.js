388f876e76ac7d379a362f6db31cadab35226d3f
Arpita Shree <shreearpita24@gmail.com>
2026-01-23 06:17:16 +0000

    implicit arrow function

--- 03_functions/05_arrow.js
+++ 03_functions/05_arrow.js
@@ -24,4 +24,37 @@ console.log(this) // Window
 when run on the browser console
 */
 
-// Arrow ()
+
+// function demon(){
+//     let demonname = "Muzan"
+//     console.log(this.demonname);// undefined, this can only be used with object context and not in function. 
+    
+// }
+
+// const demon = function(){
+//     let demonname = "Akaza"
+//     console.log(this.demonname); //undefined
+//  }
+
+/* Arrow () */
+
+const demon = () => {
+    let demonname = "Akaza"
+    console.log(this.demonname); //undefined
+     console.log(this); // {}
+    
+}
+
+//demon()
+
+//() => {} // Basic Arrow function syntax
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// Implicit Return
+const addTwo = (num1, num2) => num1 + num2
+
+
+console.log(addTwo(2,3))
