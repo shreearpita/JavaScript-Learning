fbe048d2dea04856d2bfb4ff2b0066b3ee9090f6
Arpita Shree <shreearpita24@gmail.com>
2026-01-25 09:37:27 +0000

    switch

---
+++ 04_control-flow/03_switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+
+//shift+alt+down arrow : copies the block of code.
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+
+    default:
+        break;
+}
