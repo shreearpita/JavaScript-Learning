4d34695b2337b989eb9327be1fb62fbd82d2538d
Arpita Shree <shreearpita24@gmail.com>
2026-01-25 06:10:44 +0000

    implicit scope

--- 04_control-flow/01_basic.js
+++ 04_control-flow/01_basic.js
@@ -1,9 +1,31 @@
-// if
+// if, if-else, 
 
-//Comparison operator : <,>,<=,>=,==,===
+//Comparison operator : <,>,<=,>=,==,===(checks type as well), !=, !==, 
 
-const isUserloggedIn = true
+// const isUserloggedIn = true
+// const temperature = 41
 
-if(2==2){
+// if(temperature === 41){
+//     console.log("less than 50");
+// }
+// else {
+//     console.log("temperature is greater than 50"); 
+// }
+
+// console.log("Execute");
+
+const score = 200
+
+// if(score>100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
     
-}
+// }
+
+// console.log(`User power: ${power}`);
+
+const balance = 1000
+
+// Implicit Scope
+if (balance > 500) console.log("test");
+
