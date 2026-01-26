aa5a16dba2cfe4ebe8cf9c5869ccbe532fa00f5e
Arpita Shree <shreearpita24@gmail.com>
2026-01-26 04:00:21 +0000

    while

---
+++ 05_loops/02_while.js
@@ -0,0 +1,15 @@
+// while
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ["Giyu", "Mitsuri", "Shinobu","Rengoku", "Tengen", "Muichiro", "Himejima", "Obanai", "Sanemi" ]
+
+let arr = 0
+while (arr<myArray.length) {
+    console.log(`Hashira is ${myArray[arr]}`);
+    arr = arr+1
+}
