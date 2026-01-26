61578ef0109ccbe17a09c1797d0bc7f47b9385e8
Arpita Shree <shreearpita24@gmail.com>
2026-01-26 05:08:05 +0000

    forof

---
+++ 05_loops/04_arrayloops.js
@@ -0,0 +1,17 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+    // object : on which element the loop is to be applied. (not the general object in js)
+    //console.log(num);
+}
+
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`);
+    
+}
+
